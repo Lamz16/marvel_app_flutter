0ef3238d0422c5115f3ca0439c4a3dd9fcd654f2
Lamz16 <andiku0755@gmail.com>
2024-10-08 21:33:22 +0700

    add custom colors

---
+++ lib/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color red = Color(0xFFE23636);
+  static const Color black = Color(0xFF000000);
+  static const Color darkGrey = Color(0xFF504A4A);
+  static const Color blue = Color(0xFF518CCA);
+  static const Color orange = Color(0xFFF78F3F);
+}
