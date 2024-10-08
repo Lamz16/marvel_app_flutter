c9e8743866e02dc33c46c9610ff8eb2b362c5023
Lamz16 <andiku0755@gmail.com>
2024-10-08 21:36:46 +0700

    rename detail_screen.dart

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,1 +1,10 @@
-class Detail
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
