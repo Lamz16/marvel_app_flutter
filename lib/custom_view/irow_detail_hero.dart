ca562e31dc6fa30211d751609d0d948b8058c420
Lamz16 <andiku0755@gmail.com>
2024-10-09 22:35:47 +0700

    make custom item row detail can pressed

--- lib/custom_view/irow_detail_hero.dart
+++ lib/custom_view/irow_detail_hero.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:marvel_app/my_colors.dart';
 
-
-class IrowDetailHero extends StatelessWidget{
+class IrowDetailHero extends StatelessWidget {
   const IrowDetailHero({Key? key}) : super(key: key);
 
-  Widget _buildIconWithLabel(String icon, String label, VoidCallback onPressed){
+  Widget _buildIconWithLabel(
+      String icon, String label, VoidCallback onPressed, String resource) {
     double size = 20.0; //Size of icon
     return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-      InkWell(
-      onTap: onPressed,
-      child: Image.asset(icon, width: size,height: size)),
-          const SizedBox(height: 8.0,),
-          Text(label, style: const TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),)
-        ],
-      );
-
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        InkWell(
+            onTap: onPressed,
+            child: Image.asset(icon, width: size, height: size)),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Text(label,
+            style:
+                const TextStyle(fontSize: 14.0, fontWeight: FontWeight.w700, fontFamily: 'Poppins')),
+        Text(resource,
+            style: const TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w900,
+                fontFamily: 'Poppins',
+                color: AppColors.darkGrey)),
+      ],
+    );
   }
 
   @override
@@ -24,11 +34,28 @@ class IrowDetailHero extends StatelessWidget{
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        _buildIconWithLabel('assets/icon/fire.png', 'Popularity', (){ print('Item Clicked');}),
-        _buildIconWithLabel('assets/icon/superhero.png', 'Type',(){ print('Item Clicked');}),
-        _buildIconWithLabel('assets/icon/strength.png', 'Total Strength',(){ print('Item Clicked');})
+        _buildIconWithLabel('assets/icon/fire.png', 'Popularity', () {
+          showDialog(context: context, builder: (context){
+            return const AlertDialog(
+              content: Text('The popularity is 1.800.000'),
+            );
+          });
+        }, '1.800.000'),
+        _buildIconWithLabel('assets/icon/superhero.png', 'Type', () {
+          showDialog(context: context, builder: (context){
+            return const AlertDialog(
+              content: Text('Hero type is Physical'),
+            );
+          });
+        }, 'Physical'),
+        _buildIconWithLabel('assets/icon/strength.png', 'Total Strength', () {
+          showDialog(context: context, builder: (context){
+            return const AlertDialog(
+              content: Text('The strength is 92 Powers'),
+            );
+          });
+        }, '92/100')
       ],
     );
   }
-
 }
