43271d1816079a8ac126a91c305f9ba2ba152459
Lamz16 <andiku0755@gmail.com>
2024-10-09 19:34:21 +0700

    use custom item on detail_screen.dart

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:marvel_app/custom_view/irow_detail_hero.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Captain America',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 24.0),
+              child: const IrowDetailHero(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
