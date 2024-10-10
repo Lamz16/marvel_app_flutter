cc0820af0cc3266b30468c45913dbf31bae4690e
Lamz16 <andiku0755@gmail.com>
2024-10-10 10:55:10 +0700

    add some data avengers

--- lib/data/content_data.dart
+++ lib/data/content_data.dart
@@ -1,18 +1,56 @@
 class SuperheroData {
   String imageUrl = '';
   String name = '';
+  String detail = '';
+  String popularity = '';
+  String type = '';
+  String strength = '';
 
-  SuperheroData(this.imageUrl, this.name);
+  SuperheroData(this.imageUrl, this.name, this.detail, this.popularity,
+      this.type, this.strength);
 }
 
 List<SuperheroData> superHeroList = [
   SuperheroData(
+      'assets/images/captain_america.jpeg',
+      'Captain America',
+      'Seorang prajurit yang diberikan serum Super-Soldier, memberinya kekuatan, ketahanan, dan ketangkasan luar biasa. Ia memimpin Avengers dengan moralitas kuat, menggunakan perisai vibranium sebagai senjata utama. Steve adalah simbol keberanian dan keadilan.',
+      '5.000.000',
+      'Physical',
+      '92/100'),
+  SuperheroData(
       'assets/images/captain_marvel.jpeg',
-      'Captain Marvel'),
+      'Captain Marvel',
+      'Seorang mantan pilot Angkatan Udara yang mendapatkan kekuatan super setelah berinteraksi dengan teknologi Kree. Captain Marvel memiliki kekuatan luar biasa, bisa terbang, dan memanipulasi energi kosmik. Ia adalah salah satu pahlawan paling kuat di Marvel Cinematic Universe.',
+      '3.000.000',
+      'Physical/Energy',
+      '98/100'),
   SuperheroData(
       'assets/images/hawkeye.jpeg',
-      'Hawkeye'),
+      'Hawkeye',
+      'Seorang pemanah ulung dan anggota kunci Avengers, Hawkeye adalah mantan agen S.H.I.E.L.D. yang memiliki keterampilan luar biasa dalam memanah dan pertarungan jarak dekat. Meski tidak memiliki kekuatan super, ketepatan dan kecepatan refleksnya menjadikannya aset penting dalam pertempuran. Dengan kepribadian tangguh namun penuh humor, ia sering menjadi penyeimbang emosi dalam tim.',
+      '7.000.000',
+      'Physical/Shooter',
+      '88/100'),
   SuperheroData(
       'assets/images/hulk.jpeg',
-      'Hulk')
+      'Hulk',
+      'Seorang ilmuwan yang berubah menjadi raksasa hijau berkat radiasi gamma. Saat marah, ia berubah menjadi Hulk, memiliki kekuatan fisik luar biasa dan daya tahan yang hampir tak tertandingi. Meskipun sering dianggap tak terkendali, Hulk adalah sekutu penting bagi Avengers.',
+      '4.000.000',
+      'Physical/Super Power',
+      '95/100'),
+  SuperheroData(
+      'assets/images/hulk.jpeg',
+      'Hulk',
+      'Seorang ilmuwan yang berubah menjadi raksasa hijau berkat radiasi gamma. Saat marah, ia berubah menjadi Hulk, memiliki kekuatan fisik luar biasa dan daya tahan yang hampir tak tertandingi. Meskipun sering dianggap tak terkendali, Hulk adalah sekutu penting bagi Avengers.',
+      '4.000.000',
+      'Physical/Super Power',
+      '95/100'),
+  SuperheroData(
+      'assets/images/hulk.jpeg',
+      'Hulk',
+      'Seorang ilmuwan yang berubah menjadi raksasa hijau berkat radiasi gamma. Saat marah, ia berubah menjadi Hulk, memiliki kekuatan fisik luar biasa dan daya tahan yang hampir tak tertandingi. Meskipun sering dianggap tak terkendali, Hulk adalah sekutu penting bagi Avengers.',
+      '4.000.000',
+      'Physical/Super Power',
+      '95/100'),
 ];
