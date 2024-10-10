b54faf0a271dd9f5369121c118ec634bcdc32446
Lamz16 <andiku0755@gmail.com>
2024-10-10 10:54:50 +0700

    add favorite button

---
+++ lib/custom_view/favorite_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatefulWidget{
+  const FavoriteButton({Key? key }) : super(key: key);
+
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton>{
+  bool isFavorite = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      color: Colors.white,
+      icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+      onPressed: (){
+        isFavorite = !isFavorite;
+      },
+    );
+  }
+}
