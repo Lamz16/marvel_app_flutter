c413895d667727685876754789275beb59373ef0
Lamz16 <andiku0755@gmail.com>
2024-10-10 10:55:53 +0700

    add some custom view on my_home_page.dart and navigation push

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:marvel_app/custom_view/item_avengers.dart';
+import 'package:marvel_app/data/content_data.dart';
 import 'package:marvel_app/my_colors.dart';
+import 'package:marvel_app/screen/detail_screen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -11,40 +14,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.black,
-        title: Text(widget.title, style: const TextStyle(color: Colors.white),),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: AppColors.black,
+          title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: AppColors.black,
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            final SuperheroData superHero = superHeroList[index];
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(avengers: superHero);
+                }));
+              },
+              child: ItemAvengers(avengers: superHero),
+            );
+          },
+          itemCount: superHeroList.length,
+        ));
   }
 }
